754f0dd8b8120b7802bc80c28b66037cd3cfa2a7
MikkelEngh <155554754+MikkelEngh@users.noreply.github.com>
2024-01-19 10:52:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function scrollToFooter() {
+    let footer = document.getElementById('footer');
+    footer.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToProjects() {
+    let projects = document.getElementById('projects');
+    projects.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToHobbies() {
+    let Hobbies = document.getElementById('Hobbies');
+    Hobbies.scrollIntoView({ behavior: 'smooth' });
+}
